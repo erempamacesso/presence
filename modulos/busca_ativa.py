250556efd0846b617eaba39c2d335da131513377
erempamacesso <erempamacesso@gmail.com>
2026-03-03 22:02:43 -0300

    Update busca_ativa.py

--- modulos/busca_ativa.py
+++ modulos/busca_ativa.py
@@ -30,7 +30,7 @@ def listar_arquivos_bucket(_supabase):
 
 
 def exibir_busca_ativa(supabase):
-    st.title("🔎 Ativa")
+    st.title("🔎 BUSCA Ativa")
     st.caption("Inteligência de Dados para Prevenção Escolar")
     st.markdown("---")
 
