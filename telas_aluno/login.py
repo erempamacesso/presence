2080fa930765a634f9643a0ee23d41e45723c4ae
quimicacomlardiao <bruno.mlsouza@professor.educacao.pe.gov.br>
2026-04-22 19:38:13 -0300

    ajuste no codigo

--- telas_aluno/login.py
+++ telas_aluno/login.py
@@ -56,9 +56,9 @@ def mostrar_tela_login(db_alunos):
         
         /* CORREÇÃO AQUI: Chaves duplas colocadas para o Python não quebrar */
         .logo-img {{
-            width: 80px !important;    /* Força o tamanho pequeno */
+            width: 160px !important;    /* Força o tamanho pequeno */
             height: auto !important;   /* Mantém a proporção */
-            max-width: 80px !important; 
+            max-width: 160px !important; 
             margin-bottom: 10px;
             filter: drop-shadow(0px 4px 6px rgba(0,0,0,0.5));
             display: block;
