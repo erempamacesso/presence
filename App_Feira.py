4cd2455711d77f9e5c73be0dfdf81cae81baf0f5
quimicacomlardiao <bruno.mlsouza@professor.educacao.pe.gov.br>
2026-06-17 14:55:05 -0300

    ajuste no codigo

--- App_Feira.py
+++ App_Feira.py
@@ -30,7 +30,7 @@ except ImportError as e:
 @st.cache_resource
 def init_connections():
     try:
-        # Remove espaços e aspas acidentais que causam o erro de DNS
+        # O .strip(" '\"") remove espaços e aspas (simples ou duplas) acidentais
         url_alunos = st.secrets["SUPABASE_URL_ALUNOS"].strip(" '\"")
         key_alunos = st.secrets["SUPABASE_KEY_ALUNOS"].strip(" '\"")
         url_provas = st.secrets["SUPABASE_URL_PROVAS"].strip(" '\"")
